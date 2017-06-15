b9abcf77627ee1728fe7c2455c281a86da4892f1
Jisha Abubaker <jishaa@google.com>
2017-06-15 16:21:27 -0700

    patch with formatting changes

--- appengine-java8/spanner/src/main/java/com/example/appengine/spanner/SpannerTasks.java
+++ appengine-java8/spanner/src/main/java/com/example/appengine/spanner/SpannerTasks.java
@@ -28,6 +28,7 @@ import com.google.cloud.spanner.Statement;
 import com.google.cloud.spanner.Struct;
 import com.google.common.base.Stopwatch;
 import com.google.spanner.admin.database.v1.UpdateDatabaseDdlMetadata;
+
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -53,7 +54,9 @@ class SpannerTasks {
     readOnlyTransaction
   }
 
-  /** Class to contain singer sample data. */
+  /**
+   * Class to contain singer sample data.
+   */
   static class Singer {
 
     final long singerId;
@@ -67,7 +70,9 @@ class SpannerTasks {
     }
   }
 
-  /** Class to contain album sample data. */
+  /**
+   * Class to contain album sample data.
+   */
   static class Album {
 
     final long singerId;
@@ -331,7 +336,8 @@ class SpannerTasks {
                 SpannerClient.getInstanceId(),
                 SpannerClient.getDatabaseId(),
                 Arrays.asList(
-                    "CREATE INDEX AlbumsByAlbumTitle2 ON Albums(AlbumTitle) STORING (MarketingBudget)"),
+                    "CREATE INDEX AlbumsByAlbumTitle2 "
+                    + "ON Albums(AlbumTitle) STORING (MarketingBudget)"),
                 null);
     op.waitFor();
   }
